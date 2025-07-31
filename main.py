c6bec416ac3751c58435f07b54208140f0d65188
Ben Harris <ben.harris24@gmail.com>
2025-07-30 20:05:39 -0400

    Greet function and pytest unit test

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello from your home lab via Docker!")
+from utils import greet
+
+if __name__ == "__main__":
+    print(greet("Home Lab"))
---
+++ tests/test_utils.py
@@ -0,0 +1,4 @@
+from utils import greet
+
+def test_greet():
+    assert greet("Ben") == "Hello, Ben!"
